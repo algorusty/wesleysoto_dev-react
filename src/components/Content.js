80ab83a351289a8f560fbdace092e15dbd4dce5b
algorusty <3823121+algorusty@users.noreply.github.com>
2023-11-13 08:00:30 -0600

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { marked } from 'marked';
-import resumeMarkdown from '../resume.md';
+import * as marked from 'marked';
 
 const Content = () => {
   const [markdown, setMarkdown] = useState('');
 
   useEffect(() => {
-    fetch(resumeMarkdown)
+    fetch('https://wesleysoto-dev.sfo3.cdn.digitaloceanspaces.com/Resume.md')
       .then((response) => response.text())
-      .then((text) => setMarkdown(marked.parse(text)))
+      .then((text) => setMarkdown(marked(text)))
       .catch((error) => console.error('Error fetching markdown:', error));
   }, []);
 
