480e70b07d535151e5b47b59713d3bf07dfd30d9
algorusty <3823121+algorusty@users.noreply.github.com>
2023-11-14 20:11:21 -0600

    Update workbox-config.js

--- workbox-config.js
+++ workbox-config.js
@@ -1,21 +1,8 @@
 module.exports = {
-    globDirectory: 'build/',
-    globPatterns: [
-      '**/*.{html,js,css,png,jpg,jpeg,svg}'
-    ],
-    swDest: 'build/service-worker.js',
-    navigateFallback: '/index.html',
-    clientsClaim: true,
-    skipWaiting: true,
-    runtimeCaching: [{
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 200
-        }
-      }
-    }],
-  };
-  
+  globDirectory: 'build/',
+  globPatterns: [
+    '**/*.{html,js,css,png,jpg,jpeg,svg}'
+  ],
+  swDest: 'build/service-worker.js',
+  swSrc: 'src/service-worker.js',
+};
