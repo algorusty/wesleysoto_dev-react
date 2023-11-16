6295ad0a55272ccb89781a91bbc75f7061fff0d5
algorusty <3823121+algorusty@users.noreply.github.com>
2023-11-16 07:52:29 -0600

    remove navbar

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import Navbar from './components/Navbar';
 import Content from './components/Content';
 
 function App() {
-  const drawerWidth = 0;
+	const drawerWidth = 0;
 
-  const drawerStyle = {
-    width: `${drawerWidth}px`
-  };
+	const drawerStyle = {
+		width: `${drawerWidth}px`
+	};
 
-  const mainStyle = {
-    flexGrow: 1,
-    padding: '16px',
-    marginLeft: `${drawerWidth}px`,
-  };
+	const mainStyle = {
+		flexGrow: 1,
+		padding: '16px',
+		marginLeft: `${drawerWidth}px`,
+	};
 
-  return (
-    <div style={{ display: 'flex' }}>
-      <Navbar style={drawerStyle} />
-      <main style={mainStyle}>
-        <Content />
-      </main>
-    </div>
-  );
+	return (
+		<div style={{ display: 'flex' }}>
+			<Navbar style={drawerStyle} />
+			<main style={mainStyle}>
+				<Content />
+			</main>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,23 +7,23 @@ import { BrowserRouter } from 'react-router-dom';
 import './styles/main.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('SW registered: ', registration);
-      })
-      .catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
-      });
-  });
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+                console.log('SW registered: ', registration);
+            })
+            .catch(registrationError => {
+                console.log('SW registration failed: ', registrationError);
+            });
+    });
 }
